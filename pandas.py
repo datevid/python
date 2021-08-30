b6396952ccee055af1b795db1b05922dee3eaa54
datevid <datevid@gmail.com>
2021-08-30 02:58:59 -0500

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,3 +1,7 @@
-#recorrer sólo una columna de un dataframe con el nombre del encabezado como 'name':
+#recorrer sólo una columna de un dataframe con el encabezado 'name':
 for row in df['name']:
     print(row)
+
+#recorrer todos los items de un dataframe con el encabezado 'name':
+for i, row in df.iterrows():
+    print(row['name'])
