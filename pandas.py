ec9a48934c028da338e4b7bd8d799ab9033ed9a1
datevid <datevid@gmail.com>
2021-08-30 03:01:01 -0500

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -5,3 +5,26 @@ for row in df['name']:
 #recorrer todos los items de un dataframe con el encabezado 'name':
 for i, row in df.iterrows():
     print(row['name'])
+
+
+# group by filtro_estado
+df = df.reset_index().groupby(
+    ['dependencia_destino', 'cemp_nu_dni', 'cemp_codemp', 'empleado_destino', 'filtro_estado']
+)['cantidad_por_estado'].aggregate('first').unstack()
+
+# reset hierarchical index pandas
+df = df.reset_index()
+
+# replace null or empty with the number zero
+df['RECIBIDO'].fillna(0, inplace=True)
+df['PENDIENTE'].fillna(0, inplace=True)
+
+# Change the Data Type of Columns
+df[['RECIBIDO', 'PENDIENTE']] = df[['RECIBIDO', 'PENDIENTE']].apply(pd.to_numeric)
+
+# create a new column type int
+# https://www.geeksforgeeks.org/create-a-new-column-in-pandas-dataframe-based-on-the-existing-columns/
+df['Total general'] = df['RECIBIDO'].astype(int) + df['PENDIENTE'].astype(int)
+
+# eliminamos la columna cemp_codemp del reporte
+df = df.drop(columns=['cemp_codemp'])
