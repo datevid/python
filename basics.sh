cc1d1d3d96a29381dd6593af091e444f7c4b4325
David León Vilca <datevid@gmail.com>
2022-07-11 16:29:03 -0500

    Create basics.sh

---
+++ basics.sh
@@ -0,0 +1,12 @@
+#1. Instalacion del ambiente virtual
+python -m pip install virtualenv
+
+#2. ubicarse dentro del proyecto(directorio)
+cd proyectonuevo
+
+#3. creacion del ambiente virtual
+python -m venv env
+
+#4. ingresar al ambiente virtual
+cd env/Scripts
+activate
