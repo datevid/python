9abf052f2d3e335f1efaca2711494d045d8b9bcd
David León Vilca <datevid@gmail.com>
2022-08-02 15:26:22 -0500

    Update conexion.py
    update in Python

--- conexion.py
+++ conexion.py
@@ -46,6 +46,36 @@ def getData(connConfig):
     cur.close()
     conn.close()
 
+def updateData(connConfig, coDepEqui: str, coDependencia):
+    # Establish a connection to the database by creating a cursor object
+    # The PostgreSQL server must be accessed through the PostgreSQL APP or Terminal Shell
+
+    # conn = psycopg2.connect("dbname=suppliers port=5432 user=postgres password=postgres")
+
+    # Or:
+    conn = psycopg2.connect(host=connConfig['host'], port=connConfig['port'],
+                            database=connConfig['database'],
+                            user=connConfig['user'], password=connConfig['password']);
+
+    # Create a cursor object
+    cur = conn.cursor()
+
+    # A sample query of all data from the "vendors" table in the "suppliers" database
+    sql = """UPDATE ceadm.getm_dependencia
+        SET co_dep_equi='{}'
+        WHERE co_dependencia='{}';
+    """
+    cur.execute(sql.format(coDepEqui, coDependencia))
+    conn.commit()
+    print("Data updated Successfully")
+    print("Total row affected " + str(cur.rowcount))
+
+    # Close the cursor and connection to so the server can allocate
+    # bandwidth to other requests
+    cur.close()
+    conn.close()
+
+    
 
 credentials = getCredetials();
 getData(credentials)
